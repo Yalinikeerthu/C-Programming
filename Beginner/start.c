810ae560b65ab11b5ab2b85a0879f3f1540119a5
Yalinikeerthu <37831209+Yalinikeerthu@users.noreply.github.com>
2018-03-27 16:21:12 +0530

    Update start.c

--- Beginner/start.c
+++ Beginner/start.c
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	int a;
-	cin>>a;
-	if((1<=a)||(a<=100000)){
+		cin>>a;
+	if( (1<=a) && (a<=100000) ){
 		cout<<a <<"is positive";
-		}
+			}
 		else if(a<0)
 		{
 				cout<<a <<"is negative";
 		}
-		else
+		else if(a==0)
 		{
 				cout<<a<<" is zero";
 			}
-	if(a>100000){
-			try{
-					throw a;
-				}
-		catch(int a){
-			cout<<"exception";
-		}
+		else
+		{
+		cout<<"exception";
 }
 	return 0;
 }
